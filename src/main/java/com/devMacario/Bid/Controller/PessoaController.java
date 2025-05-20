34a0f9fcad543fccdf1c2de3dd0966e4fd198f7c
Gabriel Sa <gabriellms2002@gmail.com>
2025-05-20 06:58:47 -0300

    Adicionando Uma anotacion na classe PessoaCotroller

--- src/main/java/com/devMacario/Bid/Controller/PessoaController.java
+++ src/main/java/com/devMacario/Bid/Controller/PessoaController.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.devMacario.Bid.Entitys.Pessoa;
 import com.devMacario.Bid.Service.PessoaService;
 
 
-
+@RestController
+@RequestMapping("/pessoas")
 public class PessoaController {
 	@Autowired
 	private PessoaService service;
